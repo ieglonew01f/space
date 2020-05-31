1358ef8880edeb9a979d0bdfc597bd97646a298e
Mukunda Gogoi <mukundag@qubole.com>
2020-05-31 13:48:32 +0530

    fix logical error

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -22,10 +22,3 @@ class MessageBroadcastJob < ApplicationJob
     ActionCable.server.broadcast "event_channel_#{message.for_id}", event: event
   end
 end
-
-
-object false
-object @message
-attributes :uuid, :message, :conv_id, :seen, :for_id, :by_id
-node(:incoming_message) { |message| message.for_id == current_user.id }
-child(:sender) { attributes :id, :uuid, :name, :avatar }
